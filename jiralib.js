a19d7d0de19092565986d1da44d48f3c82431c3c
Jorge Matos <jorge.matos@boardriders.com>
2022-04-28 09:21:27 -0500

    * Removed the logic that was setting the date - it seems JIRA now sets the date instead of the duration so the code wasn't being used. * Added logic to add buttons in the create issue modal window to add templates to the description field.

--- jiralib.js
+++ jiralib.js
@@ -12,10 +12,57 @@
 // ==/UserScript==
 
 (function(){
-    // no durations, REAL DATES !!!
+
+    /**
+    This function adds buttons that will appear on the "Create Issue" modal window that 
+    can be used to insert a "feature" or "bug" template into the description field. 
+    */
+    function setupDescriptionTemplateButtons() {
+        var ticketDescription = $('#description-wiki-edit');
+        var btnFeatureExists = $('#btnFeature').length > 0;
+
+        if (ticketDescription.length && !btnFeatureExists) {
+            $('#description-wiki-edit')
+                .append("<button id='btnFeature'>add feature template</button>")
+                .append("<button id='btnBug'>add bug template</button>");
+
+            $('#btnFeature').on('click', function (e) {
+                e.preventDefault();
+                ticketDescription.find('textarea').val(`{panel:title=GENERAL REQUIREMENTS}
+TBD
+{panel}
+{panel:title=TECHNICAL DESIGN}
+The design that the developer is going to implement should be here.
+DEV to fill in.
+{panel}
+{panel:title=METADATA / CONTENT / DEPLOYMENT}
+Any metadata needed for this ticket should be defined here.
+Deployment instructions should be here too.
+DEV to fill in.
+{panel}
+`);
+                return false;
+            });
+
+            $('#btnBug').on('click', function (e) {
+                 e.preventDefault();
+                 ticketDescription.find('textarea').val(`{panel:title=ISSUE}
+TBD
+{panel}
+{panel:title=ANALYSIS}
+TBD
+{panel}
+{panel:title=RESOLUTION}
+TBD
+{panel}`);
+                 return false;
+             });
+         }
+    }
+
     var realST = window.setTimeout
     window.setTimeout = function(fn,delay) {
-        $('time.livestamp').each(function(){$(this).text($(this).parent()[0].title);});
+        setupDescriptionTemplateButtons();
         return realST(fn,delay);
     };
 })();
