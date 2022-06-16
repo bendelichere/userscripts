e6ea66c30b3e1b94758ec9df6adff51f6b98dfff
Benjamin Delichere <benjamin.delichere@boardriders.com>
2022-06-16 16:52:18 +0200

    added adyenlib: auto-click load more webhooks

---
+++ adyenlib.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         ADYEN lib
+// @namespace    napali.boardriders
+// @version      22.6.16.0
+// @description  let's capture better
+// @author       Benjamin Delichere
+// @match        https://*.adyen.com/*
+// @icon         https://ca-test.adyen.com/ca/img/adyen/favicon.ico
+// @grant        none
+// @copyright    Benjamin Delichere
+// @license      X11 (MIT)
+// ==/UserScript==
+
+(function(){
+
+    var runForestRun = function () {
+        jQuery('#app-content').on('click', '.loading-button',() => {console.log('yepylacliké');autoClickLoadMoreWebhooks();});
+        autoClickLoadMoreWebhooks();
+    };
+
+    var waitForElm = function (selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {return resolve(document.querySelector(selector));}
+            const observer = new MutationObserver(mutations => {if (document.querySelector(selector)) {resolve(document.querySelector(selector));observer.disconnect();}});
+            observer.observe(document.body, {childList: true,subtree: true});
+        });
+    };
+
+    var autoClickLoadMoreWebhooks = function () {
+        waitForElm('.loading-button').then((elm) => {console.log('yepyléla');window.setTimeout(()=>{jQuery(elm).click();},500);});
+    }
+
+    runForestRun();
+})();
