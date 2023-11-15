d68061b054820bfbf7af2ef4131f76062a0fd168
Benjamin Delichere <bendelichere@users.noreply.github.com>
2023-11-15 18:16:49 +0100

    added wheel event plus some cleaning

--- awslib.js
+++ awslib.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AWS lib
 // @namespace    napali.boardriders
-// @version      23.11.15.1
+// @version      23.11.15.2
 // @icon         https://d19soru4bzonzg.cloudfront.net/assets/Prod/us-west-2/3890bfbe9059870174d403d8d884fd0e/favicon.png
 // @description  let's enhance some stuff (dashboard)
 // @author       Benjamin Delichere
@@ -37,14 +37,8 @@
         })
     }
 
-    /*
-    var myNodeList = document.querySelectorAll('li'); // grabs some <li>
-[].forEach.call(myNodeList, function (item) {
-  // :) hooray `item` can be used here
-});
-    */
-
     var hover = (element, enter, leave) => {
+        element.addEventListener('wheel', enter)
         element.addEventListener('mouseenter', enter)
         element.addEventListener('mouseleave', leave)
     }
@@ -53,16 +47,13 @@
         waitForElm(element).then((elm)=>{
             var logsInsightDivs = document.querySelectorAll(element)
             var myArrayOfDivs = []; // empty at first
-            for (var i = 0; i < logsInsightDivs.length; i++) {myArrayOfDivs.push(logsInsightDivs[i])}
-            myArrayOfDivs.forEach((e)=>{
-                myCallback(e)
-            })
+            for (var i = 0; i < logsInsightDivs.length; i++) {myCallback(logsInsightDivs[i])}
         })
     }
 
     var doAddLinkCallback = (elm) => {
         hover(elm,e=>{
-            let logsInsightNodes = e.target.querySelectorAll('div.body-row div.query-results-cell div')
+            let logsInsightNodes = e.target.querySelectorAll('div.query-results-cell div')
             for (var i = 0; i < logsInsightNodes.length; i++) {
                 //if(logsInsightNodes[i].innerHTML.match(/[A-Z]{2}-[A-Z]{2}/) !== null) {}
                 if(logsInsightNodes[i].innerHTML.match(/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/) !== null) {
@@ -77,22 +68,6 @@
         iterateAndCallback('div.cwdb-scrollGrid-container',(elm)=>{doAddLinkCallback(elm)})
         iterateAndCallback('div.cwdb-widget-preview-wrapper',(elm)=>{doAddLinkCallback(elm)})
     }
-/*        waitForElm('div.cwdb-scrollGrid-container').then((elm)=>{
-            var logsInsightDivs = document.querySelectorAll('div.cwdb-scrollGrid-container')
-            var myArrayOfDivs = []; // empty at first
-            for (var i = 0; i < logsInsightDivs.length; i++) {myArrayOfDivs.push(logsInsightDivs[i])}
-            myArrayOfDivs.forEach((elm)=>{
-                //addOrderLink(elm)
-                console.log(elm.innerHTML)
-                hover(elm,e=>{
-                    //console.log('entered',e)
-                    e.target
-                },e=>{
-                    //console.log('leaved',e)
-                })
-            })
-        })
-    }*/
 
     var runForestRun = () => {
         // add order search form in order detail page
