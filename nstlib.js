a6306c1e515ab7ca030d534c503301b662e0794b
Benjamin Delichere <bendelichere@users.noreply.github.com>
2025-07-28 09:55:23 +0200

    Update nstlib.js

--- nstlib.js
+++ nstlib.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NST lib
 // @namespace    napali.boardriders
-// @version      25.7.28.3
+// @version      25.7.28.4
 // @icon         https://manager.boardriders-staging.p.newstore.net/favicon.ico
 // @description  let's enhance some stuff (order search)
 // @author       Benjamin Delichere
